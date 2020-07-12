4f3885271b2fcb255fdb5ffd52eaf5db2790204f
Nathanael Swander <swander926@yahoo.com>
2020-07-11 20:54:04 -0400

    Added Toggled Mobile Navigation

--- public/main.js
+++ public/main.js
@@ -1,7 +1,23 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const hamburgerMenu = document.querySelector('.toggle')
+
+const navBar = document.querySelector('.navBar')
+
+hamburgerMenu.addEventListener('click', () => {
+  navBar.classList.toggle('mobileNavBar')
+}, false)
+
+document.openHamburgerMenu = function () {
+  console.log('Menu Open')
+
+  const styleBurgerContainer = document.getElementById('styleBurger')
+  console.log('styleBurgerContainer', styleBurgerContainer)
+  styleBurgerContainer.style.display = 'block'
 }
 
-document.addEventListener('DOMContentLoaded', main)
+document.closeHamburgerMenu = function () {
+  console.log('Menu Close')
+
+  const styleBurgerContainer = document.getElementById('styleBurger')
+  styleBurgerContainer.style.display = 'none'
+}
+document.addEventListener('DOMContentLoaded')
